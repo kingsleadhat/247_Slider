e05e0ca93e22e6c5796fcb7d8a4b3c31aa925ca2
kingsleadhat <jeremysparks@gmail.com>
2017-01-04 12:36:16 -0800

    Added a timer so you can protect your eardrums from a Dudio blast.

--- volume.js
+++ volume.js
@@ -5,62 +5,21 @@ $(document).ready(function() {
 	$('#logo-247').append(' \
     <center><span id="valBox" style="position: absolute; bottom: 120px; margin: 0 auto auto; left: 0; right: 0;"></span></center> \
 	<input id="volume_slider" style="position: absolute; bottom: 100px; margin: 0 auto auto; left:40%; right: 0px; z-index=101;" \
-		type="range" min="0" max="100" value="50"> \
-	</input>');
+		type="range" min="0" max="100" value="50"></input> \
+	<center><span id="timerBox" style="position: absolute; bottom: 90px; margin: 0 auto auto; left: 0; right: 0; font-size: 7pt;"></span></center>');
 
 	
 	/* Dumb console Banner */ 
-	var banner = function () {
-		console.log("        ,----,                                    ");
-		console.log("      ,/   .`|                                    ");
-		console.log("    ,`   .'  :  ,---,                             ");
-		console.log("  ;    ;     /,--.' |                             ");
-		console.log(".'___,/    ,' |  |  :                             ");
-		console.log("|    :     |  :  :  :                             ");
-		console.log(";    |.';  ;  :  |  |,--.   ,---.                 ");
-		console.log("`----'  |  |  |  :  '   |  /     \\                ");
-		console.log("    '   :  ;  |  |   /' : /    /  |               ");
-		console.log("    |   |  '  '  :  | | |.    ' / |               ");
-		console.log("    '   :  |  |  |  ' | :'   ;   /|               ");
-		console.log("    ;   |.'   |  :  :_:,''   |  / |               ");
-		console.log("    '---'     |  | ,'    |   :    |               ");
-		console.log("    ,---,.    `--''       \\   \\  / ___            ");
-		console.log("  ,'  .'  \\                `----',--.'|_          ");
-		console.log(",---.' .' |                      |  | :,'         ");
-		console.log("|   |  |: |            .--.--.   :  : ' :         ");
-		console.log(":   :  :  /   ,---.   /  /    '.;__,'  /          ");
-		console.log(":   |    ;   /     \\ |  :  /`./|  |   |           ");
-		console.log("|   :     \\ /    /  ||  :  ;_  :__,'| :           ");
-		console.log("|   |   . |.    ' / | \\  \\    `. '  : |__         ");
-		console.log("'   :  '; |'   ;   /|  `----.   \\|  | '.'|        ");
-		console.log("|   |  | ; '   |  / | /  /`--'  /;  :    ;        ");
-		console.log("|   :   /  |   :    |'--'.     / |  ,   /         ");
-		console.log("|   | ,'    \\   \\  /   `--'---'   ---`-'          ");
-		console.log("`----'       `----'                               ");
-		console.log("  .--.--.     ,---,                               ");
-		console.log(" /  /    '. ,--.' |                               ");
-		console.log("|  :  /`. / |  |  :       ,---.           .---.   ");
-		console.log(";  |  |--`  :  :  :      '   ,'\\         /. ./|   ");
-		console.log("|  :  ;_    :  |  |,--. /   /   |     .-'-. ' |   ");
-		console.log(" \\  \\    `. |  :  '   |.   ; ,. :    /___/ \\: |   ");
-		console.log("  `----.   \\|  |   /' :'   | |: : .-'.. '   ' .   ");
-		console.log("  __ \\  \\  |'  :  | | |'   | .; :/___/ \\:     '   ");
-		console.log(" /  /`--'  /|  |  ' | :|   :    |.   \\  ' .\\      ");
-		console.log("'--'.     / |  :  :_:,' \\   \\  /  \\   \\   ' \\ |   ");
-		console.log("  `--'---'  |  | ,'      `----'    \\   \\  |--\"    ");
-		console.log("            `--''                   \\   \\ |       ");
-		console.log("                                     '---\"        ");
-		console.log("            Volume Slider 1.0                     ");
-	};
-
-	banner();
-
+	console.log("Volume Slider 1.1");
+	
 	/*
 		Check if clip is playing. If so, get ID. If not, prompt in console.
 	*/
 	var clipID = undefined;
-
 	var controlButton = $('.controls').find('.play').css('display');
+	var volVal = $('#valBox');
+	var slider = $('#logo-247').find('#volume_slider');
+	var timeVal = $('#timerBox')
 
 	if (controlButton == "block") {
 		console.log('It seems you haven\'t started the stream. Press "Play".')
@@ -69,28 +28,54 @@ $(document).ready(function() {
 		console.log('You should have control of the volume now.')
 	}
 
+	/* Function to update the volume value label */
 	function showVal(newVal){
 	  document.getElementById("valBox").innerHTML=newVal;
 	}
 
-	var volVal = $('#valBox');
-	var volumey = $('#logo-247').find('#volume_slider');
+	function updateTime(newTime) {
+		document.getElementById("timerBox").innerHTML=newTime;
+	}
 
-	volumey.hide()
+	slider.hide()
 
+	/* Show/Hide volume slider if hovering over #logo-247 div */
 	$('#logo-247').hover(function() {
-	    volumey.show();
+		clipLength();
+	    slider.show();
 	    volVal.show();
-	    }, function() {
+	    timeVal.show();
+	}, function() {
 	    volVal.fadeOut('slow');
-	    volumey.hide();
+	    slider.hide();
+	    timeVal.fadeOut('slow');
 	});
 
-	volumey.on('input', function() { 
+	/* Useful millisecond to minutes/seconds bit from StackOverflow */
+	function millisToMinutesAndSeconds(millis) {
+	  var minutes = Math.floor(millis / 60000);
+	  var seconds = ((millis % 60000) / 1000).toFixed(0);
+	  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+	}
+
+	/* Check duration of clip */
+	var clipLength = function() {
+		var length = soundManager.onPosition(clipID).duration;
+		var currentPosition = soundManager.onPosition(clipID).position;
+		var currentTime = (millisToMinutesAndSeconds(currentPosition) + ' / ' + millisToMinutesAndSeconds(length));
+		updateTime(currentTime)
+	}
+
+	/* Actual slider functionality. */
+	slider.on('input', function() { 
 		var updatedVol = this.value;
 		currentSound = soundManager.soundIDs[0];
 		clipID = currentSound;
 		soundManager.setVolume(clipID, updatedVol);
 		showVal(this.value);
+		clipLength();
 	});
+
+	
+	
 });
